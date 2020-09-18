a15a428aae654706444e15badad3deb30a75fdec
kartikeya2002 <raj.karthikeya2002@gmail.com>
2020-09-18 16:19:53 +0530

    initalized a repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div>ACADEMIC COUNCIL IIT BHUBANESHWAR</div>;
+  }
+}
+
+export default App;
